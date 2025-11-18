3e1a773db675eb04d0b0759cf48c2a037f8ca93c
ArathCamacho <arath.camacho@hotmail.com>
2025-11-17 23:13:30 -0700

    fix: cambiar psycopg a psycopg2 en DATABASE_URL

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg://neondb_owner:npg_rA6bLJpsz8yY@ep-fragrant-cloud-adf9oczc-pooler.c-2.us-east-1.aws.neon.tech/ecommerce?sslmode=require"
+DATABASE_URL = "postgresql+psycopg2://neondb_owner:npg_rA6bLJpsz8yY@ep-fragrant-cloud-adf9oczc-pooler.c-2.us-east-1.aws.neon.tech/ecommerce?sslmode=require"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
